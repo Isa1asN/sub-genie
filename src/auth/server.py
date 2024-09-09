1c60bd7a15cab9997cdf08177fe4fe6beb123818
Esayas Nigussie <esayasnigusie2@gmail.com>
2024-09-09 16:49:08 +0300

    port fix and register endpoint to gateway

--- src/auth/server.py
+++ src/auth/server.py
@@ -86,4 +86,4 @@ def createJWT(username, secret, u_type):
     )
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=5005)
+    server.run(host="0.0.0.0", port=5000)
--- src/gateway/auth_svc/access.py
+++ src/gateway/auth_svc/access.py
@@ -1,5 +1,26 @@
 import os, requests
 
+def register(request):
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return None, ("email and password are required", 400)
+
+    response = requests.post(
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/register",
+        json={
+            "email": email,
+            "password": password
+        },
+        timeout=5
+    )
+    if response.status_code == 201:
+        return response.text, None
+    else:
+        return None, (response.text, response.status_code)
+
 def login(request):
     auth = request.authorization
     if not auth:
@@ -8,7 +29,8 @@ def login(request):
 
     response = requests.post(
         f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login",
-        auth=basicAuth
+        auth=basicAuth,
+        timeout=5
     )
     if response.status_code == 200:
         return response.text, None
